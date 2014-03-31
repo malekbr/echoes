5af5c1f29632301babc41df11e5d998591aeeb0a
amin_manna <amin10@meet.mit.edu>
2014-03-30 21:45:07 -0400

    cleaned up LuceneTest.java

--- LuceneDemo/src/LuceneTest.java
+++ LuceneDemo/src/LuceneTest.java
@@ -1,44 +1,20 @@
 import java.io.IOException;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.List;
-
-import org.apache.lucene.analysis.core.StopAnalyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-//import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.LongField;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
-//import org.apache.lucene.index.DirectoryReader;
-//import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
-//import org.apache.lucene.index.IndexWriterConfig;
-//import org.apache.lucene.queryparser.classic.QueryParser;
-//import org.apache.lucene.search.IndexSearcher;
-//import org.apache.lucene.search.Query;
-//import org.apache.lucene.search.ScoreDoc;
-//import org.apache.lucene.search.TopScoreDocCollector;
-//import org.apache.lucene.store.Directory;
-//import org.apache.lucene.store.RAMDirectory;
-//import org.apache.lucene.util.Version;
-
 import org.echoes.backend.LuceneDoc;
 import org.echoes.backend.MessageDoc;
 import org.echoes.backend.PostDoc;
-import com.restfb.*;
-import com.restfb.types.*;
 import com.restfb.Connection;
 import com.restfb.DefaultFacebookClient;
 import com.restfb.FacebookClient;
 import com.restfb.Parameter;
 import com.restfb.types.Post;
-import com.restfb.types.User;
 
 public class LuceneTest {
 	private static final String ACCESS_TOKEN = PrivateResource.accessToken;
