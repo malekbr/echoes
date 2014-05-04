24a5b15979859717ef667adba489661069698ff6
amin10 <manna@mit.edu>
2014-05-04 15:53:35 -0400

    Delete PrivateResource.java

--- LuceneDemo/src/PrivateResource.java
+++
@@ -1,6 +0,0 @@
-public class PrivateResource {
-	public static String accessToken = "CAACEdEose0cBAGETlNa7ITw0JZBLNZAZBDpYSdcIePmk8rAPe8aBCiPJHBJrZCBZAYA2rllXCk3O8vwu6Djm4kW7SsDIPIPmnZBnZCX15i5ZC5p8kcw86sjK5bTBiiSneYdSdkDOe3Ne46jal8BZCViO5ZCKzym9WajyZCB0eWzowdYXzuGIyk5lX0ZB5AoaaR9O7f7YpL7z0v0HUQZDZD";
-	public static String appId = "1475961842619811";
-	public static String appSecret = "41113339e01259c5b5c1428df7871e36";
-	public static String me = "145634995501895";
-}
